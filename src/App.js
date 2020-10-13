8b061f30149bf78ba3251e8e931a1f658a4e9678
Gabehd94 <gabehd94@gmail.com>
2020-10-13 10:26:38 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import Q1 from "./questions/q1"
 import './App.css';
 
-function App() {
-  return (
+export default function App() {
+
+  const questions = [
+    {
+      questionText: "Is the account data entry?",
+      answerOptions: [
+        {answerText: "No", isCorrect: false},
+        {answerText: "Yes", isCorrect: true},
+      ],
+    },
+    {
+      questionText: "Is this customer 1 or 2?",
+      answerOptions: [
+        {answerText: "No", isCorrect: false},
+        {answerText: "Yes", isCorrect: true},
+      ],
+    },
+    {
+      questionText: "Is it someone with a Power of Attorney or Conservatorship for the Customer?",
+      answerOptions: [
+        {answerText: "No", isCorrect: false},
+        {answerText: "Yes", isCorrect: true},
+      ],
+    },
+    {
+      questionText: "Is it someone with a signed 'Name Add Form' on file?",
+      answerOptions: [
+        {answerText: "No", isCorrect: false},
+        {answerText: "Yes", isCorrect: true},
+      ],
+    },
+    {
+      questionText: "Is it someone who is verbally authorized by a verified account holder to speak with Alder?",
+      answerOptions: [
+        {answerText: "No", isCorrect: false},
+        {answerText: "Yes", isCorrect: true},
+      ],
+    },
+    { //this is object number 5
+      questionText: "VERIFIED",
+      answerOptions: [
+
+      ],
+    },
+    { //this is object number 6
+      questionText: "You can tell the customer their verbal password.",
+      answerOptions: [
+
+      ],
+    },
+  ]
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handleAnswerButton = () => {
+    const nextQuestion = currentQuestion + 1;
+    setCurrentQuestion(nextQuestion);
+  }
+
+  return (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{questions[currentQuestion].questionText}</h1>
+      
+      <div className="answerSection">
+        {questions[currentQuestion].answerOptions.map((answerOption)=> (
+        <button onClick={handleAnswerButton}>{answerOption.answerText}</button>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+
+
